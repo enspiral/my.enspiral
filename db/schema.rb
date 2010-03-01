41a431e7bdc5dc95737fcd14d7d62cea82292adc
Samson Ootoowak <ootoovak@gmail.com>
2010-03-02 10:31:22 +1300

    Fixed the relationships for person and project as well as getting rid of the un-needed projects layout.

--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100301021622) do
+ActiveRecord::Schema.define(:version => 20100301212333) do
 
   create_table "people", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "job_title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "worked_ons", :force => true do |t|
+    t.integer  "person_id"
+    t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
