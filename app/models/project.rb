e8df2083d646800347e666fd5ff1f96adae6bb75
Joshua Vial <joshua@enspiral.com>
2010-03-02 10:53:17 +1300

    testing has many through association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
+  has_many :worked_on
   has_many :people, :through => :worked_on
 end
--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "worked on relationships" do
+    setup do 
+      @lachlan = people(:lachlan)
+      @sam = people(:sam)
+      @project = projects(:enspiral_site)
+    end
+    
+    should "link people to project" do
+      assert @lachlan.projects.include?(@project)
+      assert @sam.projects.include?(@project)
+    end
+
+    should "link project to person" do
+      assert @project.people.include?(@lachlan)
+      assert @project.people.include?(@sam)
+    end
+
   end
 end
