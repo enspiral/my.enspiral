e95c6ea61140dbbb263817e3d5917eef5cf9cbc5
Samson Ootoowak <ootoovak@gmail.com>
2010-03-02 16:57:07 +1300

    Added in paperclip functionality to the projects model so we can upload pictures for a project.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100302014810) do
+ActiveRecord::Schema.define(:version => 20100302023839) do
 
   create_table "people", :force => true do |t|
     t.string   "first_name"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(:version => 20100302014810) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "worked_ons", :force => true do |t|
