0cff62b6020a2caf43ae32afe367ae478192f983
Samson Ootoowak <ootoovak@gmail.com>
2010-03-02 15:17:26 +1300

    Added destroy to the model relationship of person and project.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
   is_gravtastic! :rating => 'PG'
   
-  has_many :worked_on
+  has_many :worked_on, :dependent => :destroy
   has_many :projects, :through => :worked_on
   
   validates_presence_of :email
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  has_many :worked_on
+  has_many :worked_on, :dependent => :destroy
   has_many :people, :through => :worked_on
 end
