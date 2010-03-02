3a7494f63d652fb446af0025a732bfef8bbcae0e
Samson Ootoowak <ootoovak@gmail.com>
2010-03-02 16:57:07 +1300

    Added in paperclip functionality to the projects model so we can upload pictures for a project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ActiveRecord::Base
   has_many :worked_on, :dependent => :destroy
   has_many :people, :through => :worked_on
+
+  has_attached_file :image, :styles => {
+    :thumb=> "100x100#",
+    :medium  => "256x256>"
+  }
 end
