87afa99782781b015198c42f8ba71212385c6148
lachlanscott <lachlan.scott@me.com>
2010-03-04 14:49:16 +1300

    Added Account creation through Person model on create

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -6,4 +6,17 @@ class PersonTest < ActiveSupport::TestCase
 		lachlan = Person.find_by_email "lachlan.scott@me.com"
 		assert !lachlan.nil?
 	end
+	
+	should "have account on create" do
+		p = Person.new
+		assert_equal nil, p.account
+		
+		p.email = "test@somewhere.com"
+		
+		assert_difference 'Account.count' do
+			p.save
+		end
+		p.reload
+		assert !p.account.nil?
+	end
 end
