7a91fe5debe8f9e641bcda6679620eb6c609d1a3
lachlanscott <lachlan.scott@me.com>
2010-03-05 10:22:42 +1300

    Added link from people/show to account/edit. NB. Page links generally require attention.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -16,6 +16,8 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
     @projects = @person.projects
 
+	@account = @person.account
+
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @person }
