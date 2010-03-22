fac4aac80884c65143b34f39f529a19eedc5bb59
Joshua Vial <joshua@enspiral.com>
2010-03-22 16:29:50 +1300

    fixing login

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  should "get new" do
+    get :new
+    assert_response :success
+    assert !assigns(:user_session).nil?
   end
+
+  should  "redirect correctly when admin" do
+    post :create, :user_session => {:email => 'joshua@enspiral.com', :password => 'password'}
+    assert_redirected_to admin_people_url
+  end
+
+  should "redirect correctly when staff" do
+    post :create, :user_session => {:email => 'sam@enspiral.com', :password => 'password'}
+    assert_redirected_to root_url
+  end
+
+  should "show error when password invalid" do
+    post :create, :user_session => {:email => 'sam@enspiral.com', :password => 'wrong-password'}
+    assert_response :success
+    assert_select '.error'
+  end
+
 end
