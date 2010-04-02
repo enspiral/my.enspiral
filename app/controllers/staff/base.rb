3d0c120aab254a8c949a0d403fe917ffb8330e73
Joshua Vial <joshua@enspiral.com>
2010-04-02 15:02:01 +1300

    basic admin and staff sections

---
+++ app/controllers/staff/base.rb
@@ -0,0 +1,15 @@
+class Staff::Base < ApplicationController
+#  before_filter :login_required
+#  before_filter :admin_only
+  
+  layout "admin"
+  
+  private
+  
+#    def admin_only
+#      unless current_user.admin?
+#        redirect_to '/'
+#      end
+#    end
+end
+
---
+++ test/unit/helpers/staff/dashboard_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Staff::DashboardHelperTest < ActionView::TestCase
+end
