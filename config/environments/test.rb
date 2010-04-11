ca85938632575bf5c8749c0fbac7eb27439773b4
Joshua Vial <joshua@enspiral.com>
2010-04-11 13:42:24 +1200

    adding cucumber and rspec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,3 +27,5 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 SITE_DOMAIN="localhost:3000"
+
+  config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
