a5ac37123ee7e9a3cb412d9b136cfd6ab73152d5
Samson Ootoowak <ootoovak@gmail.com>
2010-05-07 15:50:34 +1200

    Backend savage beast working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include SavageBeast::UserInit
+  
   has_one :person
 
   validates_presence_of :role
@@ -9,13 +11,27 @@ class User < ActiveRecord::Base
     a.login_field = 'email'
   end
 
+  def staff?
+    role.to_sym == :staff
+  end
+
+  # BEGIN Required for savage-beast
+  def display_name
+		person.username
+	end
 
   def admin?
     role.to_sym == :admin
   end
 
-  def staff?
-    role.to_sym == :staff
+  def currently_online
+    false
   end
 
+  #implmement to build search coondtitions
+	# def build_search_conditions(query)
+		# # query && ['LOWER(display_name) LIKE :q OR LOWER(login) LIKE :q', {:q => "%#{query}%"}]
+		# query
+	# end
+  # END Required for savage-beast
 end
