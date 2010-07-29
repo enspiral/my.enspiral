f9f5dab6a3e09004c6b0723604c14b35c2530bb3
Joshua Vial <joshua@enspiral.com>
2010-07-29 22:07:46 +1200

    totalled transactions

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,4 +8,22 @@ class Account < ActiveRecord::Base
     sum
   end
 
+  def transactions_with_totals
+    return []  if transactions.nil?
+    totalled_transactions = []
+    total = 0
+
+    transactions.reverse.each do |transaction|
+      if total == 0
+        total = transaction.amount
+      else
+        total += transaction.amount
+      end
+
+      totalled_transactions << [transaction, total]
+    end
+
+   totalled_transactions.reverse!
+  end
+
 end
