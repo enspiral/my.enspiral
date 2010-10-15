43b36d16fc61eb0be02f2b0733b24a2e0c287ec4
Aries Andrada <aries.andrada@gmail.com>
2010-10-16 08:51:34 +1300

    integrated layout

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   config.gem 'authlogic'
   config.gem 'RedCloth'
   config.gem 'hoptoad_notifier'
+  config.gem 'haml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
