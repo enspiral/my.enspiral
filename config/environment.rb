46a2057172869e6925c3574a6ae8f5beaa5d56da
Joshua Vial <joshua@enspiral.com>
2010-10-16 13:03:55 +1300

    fixing bundler setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file
 require 'rubygems'
 require 'bundler'
+Bundler.setup
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
