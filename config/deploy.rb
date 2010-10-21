f1da7c17520e235182c25e72680096aac266b566
Joshua Vial <joshua@enspiral.com>
2010-10-21 18:45:16 +1300

    removing multistage deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,15 +3,24 @@ set :user, application
 set :repository,  "git@github.com:enspiral/enspiral.git"
 set :scm, :git
 
-set :deploy_to, "/home/#{application}/production"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
-set :branch, "production"
+task :production do
+  set :deploy_to, "/home/#{application}/production"
+  set :branch, "production"
+  set :rails_env, 'production'
+end
 
-role :app, "mars2.sinuous.net"
-role :web, "mars2.sinuous.net"
-role :db,  "mars2.sinuous.net", :primary => true
+task :staging do
+  set :deploy_to, "/home/#{application}/staging"
+  set :branch, "master"
+  set :rails_env, 'staging'
+end
+
+role :app, "linode.enspiral.com"
+role :web, "linode.enspiral.com"
+role :db,  "linode.enspiral.com", :primary => true
 
 namespace :deploy do
   # Using Phusion Passenger
@@ -29,7 +38,7 @@ namespace :deploy do
   
   desc "bundle gems"
   task :bundle do
-    run "cd #{release_path} && RAILS_ENV=#{rails_env} && bundle install #{shared_path}/gems/cache"
+    run "cd #{release_path} && RAILS_ENV=#{rails_env} && bundle install #{shared_path}/gems/cache --deployment"
   end
 end
 
@@ -37,10 +46,6 @@ after "deploy:update_code" do
   deploy.symlink_configs
   deploy.bundle
 end
-
-
-
-
 
 Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
   $: << File.join(vendored_notifier, 'lib')
