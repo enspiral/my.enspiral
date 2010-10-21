89391452ec5dbfbfaa26ee164bd0d93282d92d01
Aries Andrada <aries.andrada@gmail.com>
2010-10-22 07:31:26 +1300

    updated gravtastic

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
-  is_gravtastic! :rating => 'PG'
+  include Gravtastic
+  
+  gravtastic :rating => 'PG'
   
   has_many :worked_on, :dependent => :destroy
   has_many :projects, :through => :worked_on
