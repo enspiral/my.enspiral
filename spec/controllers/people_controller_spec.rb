270061d786866eb99bc8d4b2b1910c450cec79df
Joshua Vial <joshua@enspiral.com>
2010-10-26 12:13:25 +1300

    refactoring specs

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -4,29 +4,31 @@ describe PeopleController do
   setup :activate_authlogic
 
   before(:each) do
-    login_as User.make(:staff)
+    @person = Person.make!(:staff)
+    login_as @person.user
     request.env["HTTP_REFERER"] = 'http://somewhere.com'
   end
 
-  it "should get edit" do
-    person = mock_model(Person)
-    Person.should_receive(:find).and_return person
-    get :edit
-    response.should be_success
+  describe "editing" do
+    it "successfully load" do
+      get :edit
+      response.should be_success
+    end
+    it "should load the current user" do
+      get :edit
+      assigns(:person).should == @person
+    end
   end
 
   it "should get show" do
-    person = mock_model(Person).as_null_object
-    Person.should_receive(:find).and_return person
+    Person.should_receive(:find).and_return @person
     get :show
     response.should be_success
   end
 
   it "should update person" do
-    person = Person.make
-    person.save!
-    person.should_receive(:update_attributes).and_return true
-    Person.should_receive(:find).and_return person
+    @person.should_receive(:update_attributes).and_return true
+    Person.should_receive(:find).and_return @person
     post :update
     flash[:notice].should_not be_empty
   end
