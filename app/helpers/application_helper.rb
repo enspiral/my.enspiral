fcf0bc898a55c47c7469ce13e5fc4e4133e5a754
allansideas <allansideas@gmail.com>
2010-10-26 14:31:18 +1300

    added gravatar helper method, takes size and person displays border size defined in css for small medium and large, added method to admin, and staff views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,16 @@ module ApplicationHelper
     true  # Don't show "no gravatar" if the service is down or slow
   end
 
+  def display_gravatar(size = 80, person = null)
+    if size > 100 && size < 255
+      @border_size = "medium"
+    elsif size >= 255
+      @border_size = "large"
+    else
+      @border_size = "small"
+    end
+
+    image_tag(person.gravatar_url(:size => size), :class=>"gravatar #{person.user.role} #{@border_size}")
+  end
+
 end
