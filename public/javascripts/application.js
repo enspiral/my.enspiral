2f1d1ff8539222cc06f828a6832425c150d98046
Aries Andrada <aries.andrada@gmail.com>
2010-11-02 19:55:07 +1300

    fixed broken city page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,5 @@
 (function($) {
-    $(document).ready(function() {
-        $('a.notice_summary').toggle(
-            function() {
-                notice_text = $(this).parent().find('div.notice_text');
-                notice_text.fadeIn();
-                notice_text.slideDown();
-            },
-            function() {
-                notice_text = $(this).parent().find('div.notice_text');
-                notice_text.fadeOut();
-                notice_text.slideUp();
-            }
-            );
+  $(document).ready(function() {
     $('.flash.notice').delay('3000').slideUp('slow');
     //Ajax listeners
     //Staff Page
@@ -34,7 +22,31 @@
           }
         }
       });
-  });
-
     });
+    
+    $('a.notice_summary').toggle(
+      function() {
+        notice_text = $(this).parent().find('div.notice_text');
+        notice_text.fadeIn();
+        notice_text.slideDown();
+      },
+      function() {
+        notice_text = $(this).parent().find('div.notice_text');
+        notice_text.fadeOut();
+        notice_text.slideUp();
+      }
+    );
+    
+    $('select#person_country_id').change(function(e) {
+      country_id = $(this).val();
+      if (country_id == '') {
+        $('select#person_city_id').html('<option value=""></option>');
+      } else {
+        jQuery.get('/update_profile/get_cities/' + country_id, function(data) {
+          $('select#person_city_id').html(data);
+        });
+      }
+    });
+
+  });
 })(jQuery);
