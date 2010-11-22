a6321325b36765fb9db285c026a4d062f024eecf
Samson Ootoowak <ootoovak@gmail.com>
2010-11-23 11:01:07 +1300

    Update steak helper.

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -1,8 +1,17 @@
-require File.dirname(__FILE__) + "/../spec_helper"
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 require "steak"
 require 'capybara/rails'
 
-RSpec.configuration.include Capybara, :type => :acceptance
+module Steak::Capybara
+  include Rack::Test::Methods
+  include Capybara
+  
+  def app
+    ::Rails.application
+  end
+end
+
+RSpec.configuration.include Steak::Capybara, :type => :acceptance
 
 # Put your acceptance spec helpers inside /spec/acceptance/support
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
