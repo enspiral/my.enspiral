bc7d50c690528578616fc5df7c7431a294936054
Samson Ootoowak <ootoovak@gmail.com>
2010-11-22 14:54:20 +1300

    Add concept of public and private people

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,11 +12,11 @@ class PagesController < ApplicationController
 
   def index
     @feeds = FeedEntry.latest
-    @people = Person.featured
+    @people = Person.public.featured
 
     if @people.length < MATRIX
       spaces_left = MATRIX - @people.length 
-      more_people = Person.with_gravatar - @people
+      more_people = Person.public.with_gravatar - @people
       more_people = more_people.sort_by{ rand }.slice(0, spaces_left)
       @people += more_people
     end
