e31f7347c30bafb2db99fa9618f86755713c3431
Samson Ootoowak <ootoovak@gmail.com>
2010-11-25 10:21:27 +1300

    Use database cleaner as the Machinist database did not seem to be working

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'steak'
   gem 'capybara'
+  gem 'database_cleaner'
   gem 'spork'
   gem 'machinist', '>= 2.0.0.beta2'
   gem 'faker'
--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -1,9 +1,4 @@
 module HelperMethods
-  RSpec.configure do |config|
-    config.before(:each) do
-      Machinist.reset_before_test
-    end
-  end
 end
 
 RSpec.configuration.include HelperMethods, :type => :acceptance
