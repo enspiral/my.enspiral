d627de6e2ce12dbe0db10356229498fb7bce83ea
Joshua Vial <joshua@enspiral.com>
2011-02-19 17:43:37 +1300

    fixing devise migrations

--- db/migrate/20101123095831_remove_authlogic_from_user.rb
+++ db/migrate/20101123095831_remove_authlogic_from_user.rb
@@ -1,13 +1,9 @@
 class RemoveAuthlogicFromUser < ActiveRecord::Migration
   def self.up
-    remove_column :users, :crypted_password
-    remove_column :users, :password_salt
     remove_column :users, :persistence_token
   end
 
   def self.down
-    add_column :users, :persistence_token, :string
-    add_column :users, :password_salt, :string
     add_column :users, :crypted_password, :string
   end
 end
