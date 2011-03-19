9721ab38753b64e3b85162136885431cff83dc9d
allansideas <allansideas@gmail.com>
2011-03-20 10:23:51 +1300

    add rest to badges

---
+++ db/migrate/20110319212004_create_badges.rb
@@ -0,0 +1,13 @@
+class CreateBadges < ActiveRecord::Migration
+  def self.up
+    create_table :badges do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :badges
+  end
+end
