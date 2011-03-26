0e33ed3eece616c2857b60baaed5e42318d101f5
Joshua Vial <joshua@enspiral.com>
2011-03-26 22:19:40 +1300

    removing teams and worked_on

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110326084436) do
+ActiveRecord::Schema.define(:version => 20110326091325) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "person_id"
@@ -60,6 +60,15 @@ ActiveRecord::Schema.define(:version => 20110326084436) do
     t.datetime "updated_at"
   end
 
+  create_table "forums", :force => true do |t|
+    t.string  "name"
+    t.string  "description"
+    t.integer "topics_count",     :default => 0
+    t.integer "posts_count",      :default => 0
+    t.integer "position"
+    t.text    "description_html"
+  end
+
   create_table "invoice_allocations", :force => true do |t|
     t.integer  "person_id"
     t.integer  "invoice_id"
@@ -84,6 +93,19 @@ ActiveRecord::Schema.define(:version => 20110326084436) do
     t.integer  "number"
   end
 
+  create_table "moderatorships", :force => true do |t|
+    t.integer "forum_id"
+    t.integer "user_id"
+  end
+
+  add_index "moderatorships", ["forum_id"], :name => "index_moderatorships_on_forum_id"
+
+  create_table "monitorships", :force => true do |t|
+    t.integer "topic_id"
+    t.integer "user_id"
+    t.boolean "active",   :default => true
+  end
+
   create_table "notices", :force => true do |t|
     t.integer  "person_id"
     t.string   "summary"
@@ -99,7 +121,6 @@ ActiveRecord::Schema.define(:version => 20110326084436) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
-    t.integer  "team_id"
     t.integer  "user_id"
     t.decimal  "base_commission", :precision => 10, :scale => 2, :default => 0.2
     t.boolean  "has_gravatar",                                   :default => false
@@ -113,6 +134,20 @@ ActiveRecord::Schema.define(:version => 20110326084436) do
     t.string   "skype"
     t.boolean  "active",                                         :default => true
   end
+
+  create_table "posts", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "topic_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "forum_id"
+    t.text     "body_html"
+  end
+
+  add_index "posts", ["forum_id", "created_at"], :name => "index_posts_on_forum_id"
+  add_index "posts", ["topic_id", "created_at"], :name => "index_posts_on_topic_id"
+  add_index "posts", ["user_id", "created_at"], :name => "index_posts_on_user_id"
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -140,13 +175,24 @@ ActiveRecord::Schema.define(:version => 20110326084436) do
     t.datetime "updated_at"
   end
 
-  create_table "teams", :force => true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "city"
+  create_table "topics", :force => true do |t|
+    t.integer  "forum_id"
+    t.integer  "user_id"
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "hits",         :default => 0
+    t.integer  "sticky",       :default => 0
+    t.integer  "posts_count",  :default => 0
+    t.datetime "replied_at"
+    t.boolean  "locked",       :default => false
+    t.integer  "replied_by"
+    t.integer  "last_post_id"
   end
+
+  add_index "topics", ["forum_id", "replied_at"], :name => "index_topics_on_forum_id_and_replied_at"
+  add_index "topics", ["forum_id", "sticky", "replied_at"], :name => "index_topics_on_sticky_and_replied_at"
+  add_index "topics", ["forum_id"], :name => "index_topics_on_forum_id"
 
   create_table "transactions", :force => true do |t|
     t.integer  "account_id"
@@ -160,16 +206,16 @@ ActiveRecord::Schema.define(:version => 20110326084436) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
-    t.string   "email",                               :default => "",   :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "",   :null => false
-    t.string   "password_salt",                       :default => "",   :null => false
+    t.string   "email",                :default => "",   :null => false
+    t.string   "encrypted_password"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
+    t.integer  "sign_in_count",        :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -177,24 +223,16 @@ ActiveRecord::Schema.define(:version => 20110326084436) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.integer  "failed_attempts",                     :default => 0
+    t.integer  "failed_attempts",      :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.string   "authentication_token"
-    t.boolean  "active",                              :default => true
+    t.boolean  "active",               :default => true
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
-
-  create_table "worked_ons", :force => true do |t|
-    t.integer  "person_id"
-    t.integer  "project_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "description"
-  end
 
 end
