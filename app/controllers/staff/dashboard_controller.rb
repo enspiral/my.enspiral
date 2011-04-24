a22631b62a2e2a670571fe93939c3f23ddd42b91
Samson Ootoowak <ootoovak@gmail.com>
2011-04-24 14:40:09 +1200

    Initial clean up of dashboard before updating tables and adding graphs.

--- app/controllers/staff/dashboard_controller.rb
+++ app/controllers/staff/dashboard_controller.rb
@@ -1,9 +1,13 @@
 class Staff::DashboardController < Staff::Base
-
-  def index
+  def dashboard
     @latest_badge = BadgeOwnership.last
     @person = current_person
-    render :layout => 'application'
+
+    @recent_transactions = @person.account.transactions_with_totals[0..4]
+    @invoice_allocations = @person.invoice_allocations.pending
   end
 
+  def transactions
+    @transactions = current_person.account.transactions_with_totals
+  end
 end
