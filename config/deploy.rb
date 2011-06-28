ec33ebd4f3f8e07223f4ca7ce67a156498581c79
Samson Ootoowak <ootoovak@gmail.com>
2011-06-28 18:05:10 +1200

    Update deploy script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,56 +1,56 @@
-set :application, "enspiral"
-set :user, application
-set :repository,  "git@github.com:enspiral/enspiral.git"
-set :scm, :git
+set :application, 'enspiral'
+set :user,        'enspiral'
+set :repository,  'git@github.com:enspiral/#{application}.git'
+set :scm,         :git
 
-set :deploy_via, :remote_cache
-set :use_sudo, false
-
-task :production do
-  set :deploy_to, "/home/#{application}/production"
-  set :branch, "production"
-  set :rails_env, 'production'
-end
+set :deploy_via,  :remote_cache
+set :use_sudo,    false
 
 task :staging do
-  set :deploy_to, "/home/#{application}/staging"
-  set :branch, "staging"
+  set :domain,    'staging.enspiral.com'
+  set :branch,    'master'
   set :rails_env, 'staging'
+  set :deploy_to, '/home/#{user}/staging'
+  
+  role :app, domain
+  role :web, domain
+  role :db,  domain, :primary => true
 end
 
-role :app, "linode.enspiral.com"
-role :web, "linode.enspiral.com"
-role :db,  "linode.enspiral.com", :primary => true
+task :production do
+  set :domain,    'enspiral.com'
+  set :branch,    'production'
+  set :rails_env, 'production'
+  set :deploy_to, '/home/#{user}/production'
+  
+  role :app, domain
+  role :web, domain
+  role :db,  domain, :primary => true
+end
 
 namespace :deploy do
-  # Using Phusion Passenger
   [:stop, :start, :restart].each do |task_name|
+    desc 'Touch restart.txt so server is restarted.'
     task task_name, :roles => [:app] do
-      run "cd #{current_path} && touch tmp/restart.txt"
+      run 'cd #{current_path} && touch tmp/restart.txt'
     end 
-  end 
-  
+  end
+
+  desc 'Make system links'
   task :symlink_configs do
     run %( cd #{release_path} &&
-      ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml 
+      ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml
     )
-  end 
-  
-  desc "bundle gems"
-  task :bundle do
-    run "cd #{release_path} && RAILS_ENV=#{rails_env} && bundle install #{shared_path}/gems/cache --deployment"
   end
 end
 
-after "deploy:update_code" do
+after 'deploy:update_code' do
   deploy.symlink_configs
   deploy.bundle
 end
 
-Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
-  $: << File.join(vendored_notifier, 'lib')
-end
-
+require 'config/boot'
+require 'bundler/capistrano'
 require 'hoptoad_notifier/capistrano'
 require 'whenever/capistrano'
 
