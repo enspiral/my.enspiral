6348092afa1b9b52b54501b9ad80ec43d4bff61d
Samson Ootoowak <ootoovak@gmail.com>
2011-06-29 23:13:08 +1200

    Remove test folder as we user rspec.

--- config/application.rb
+++ config/application.rb
@@ -24,14 +24,11 @@ module Enspiral
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'UTC'
+    # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery rails application)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
--- test/unit/helpers/staff/dashboard_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Staff::DashboardHelperTest < ActionView::TestCase
-end
