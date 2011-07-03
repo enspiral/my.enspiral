be3afcce52db799deda8be2274e0c906606c7df8
Samson Ootoowak <ootoovak@gmail.com>
2011-07-04 11:45:43 +1200

    Got tooltips and graphs working again.

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -5,20 +5,6 @@ class Admin::TransactionsController < Admin::Base
     @pending_total = sum_allocations(InvoiceAllocation.pending)
   end
 
-  def balances
-    if current_user.admin?
-      person = Person.find(params[:person_id])
-    else
-      person = current_person
-    end
-
-    transactions = Transaction.transactions_with_totals(person.account.transactions)
-    transactions = transactions[0..(params[:limit].to_i - 1)] if params[:limit]
-
-    balances = transactions.map { |t, b| [(t.created_at.to_i * 1000).to_s, b.to_s] } 
-    render :json => balances
-  end
-
   private
 
   def sum_allocations(allocations)
