c2d207825809c80d1f024a3aaf93e74f9ebb60e6
allansideas <allansideas@gmail.com>
2011-08-19 14:30:24 +1200

    use 0 as completion flag so if has score then finished

--- app/controllers/staff/dashboard_controller.rb
+++ app/controllers/staff/dashboard_controller.rb
@@ -3,7 +3,8 @@ class Staff::DashboardController < Staff::Base
     @person = current_person
 
     @latest_badge = BadgeOwnership.last
-    @current_user_goals = Goal.where("person_id = ? AND date >= ?", current_user.person.id, Time.now - 1.days)
+    #score 1-5 = complete, 0 is flag for incomplete sound stupid?
+    @current_user_goals = Goal.where("person_id = ? AND date >= ? AND score == ?", current_user.person.id, Time.now - 1.days, 0)
     @transactions = Transaction.transactions_with_totals(@person.account.transactions)[0..9] if @person.account
     @invoice_allocations = @person.invoice_allocations.pending
     @pending_total = @person.pending_total
