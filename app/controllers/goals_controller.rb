8f5850f6d79aee75990095f5a8e7c1170f343c50
allansideas <allansideas@gmail.com>
2011-08-19 14:45:05 +1200

    sql fix

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,7 +3,7 @@ class GoalsController < ApplicationController
   #before_filter :authenticate_user!, :except => :show
 
   def index
-    @current = Goal.where("date >= ? AND score == ?", Time.now - 1.days, 0)
+    @current = Goal.where("date >= ? AND score = ?", Time.now - 1.days, 0)
     @past = Goal.where("date <= ? OR score > ?", Time.now - 1.days, 0)
     @goal = Goal.new
   end
