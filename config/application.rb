98b8ceabba703360c61e98bd987ad49fee0edd4c
Samson Ootoowak <ootoovak@gmail.com>
2011-08-31 20:24:16 +1200

    Had an extra end in the application.rb file.

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,6 @@ module Enspiral
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-  end
 
     config.generators do |g|
       g.fixture_replacement :machinist
