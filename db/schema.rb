56d0fc69285d8d2d176c872721898821d604a8c4
Joshua Vial <joshua@enspiral.com>
2011-12-07 16:32:32 +1300

    updating index

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,14 +11,6 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20120220214949) do
-
-  create_table "account_permissions", :force => true do |t|
-    t.integer  "account_id"
-    t.integer  "person_id"
-    t.string   "role"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "accounts", :force => true do |t|
     t.integer  "person_id"
