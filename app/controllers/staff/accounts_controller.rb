e13bef261538a93666e520df6b33ea9c97d040e4
Joshua Vial <joshua@enspiral.com>
2011-12-20 00:02:06 +1300

    basic rehash of accounts

--- app/controllers/staff/accounts_controller.rb
+++ app/controllers/staff/accounts_controller.rb
@@ -1,2 +1,37 @@
-class Staff::AccountsController < ApplicationController
+class Staff::AccountsController < Staff::Base
+  before_filter :find_account, :only => [:show, :balances, :history]
+
+  def index
+    if current_user.admin?
+      @accounts = Account.active
+    else
+      @accounts = Account.active.where("person_id IS NULL OR person_id = #{current_person.id}")
+    end
+  end
+
+  def show
+  end
+
+  def balances
+    transactions = Transaction.transactions_with_totals(@account.transactions)
+    transactions = transactions[0..(params[:limit].to_i - 1)] if params[:limit]
+
+    balances = transactions.map { |t, b| [(t.date.to_time.to_i * 1000).to_s, b.to_s] } 
+    render :json => balances
+  end
+
+  def history
+    @transactions = Transaction.transactions_with_totals(@account.transactions)
+    @pending_total = @account.pending_total
+  end
+
+  private
+
+  def find_account
+    #TODO put auth 
+    id = params[:id] || params[:account_id]
+    @account = Account.find(id)
+  end
+
+
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -5,12 +5,44 @@ class Account < ActiveRecord::Base
   validates_uniqueness_of :person_id, :allow_nil => true
   validates_uniqueness_of :project_id, :allow_nil => true
 
+  scope :with_projects, where("project_id IS NOT NULL")
+  scope :active, where(:active => true)
+
   has_many :transactions, :order => "date DESC, amount DESC"
   has_many :account_permissions
+  has_many :invoice_allocations
 
   def calculate_balance 
     sum = transactions.sum('amount')
     update_attribute(:balance, sum)
     sum
   end
+
+  def name
+    if read_attribute(:name)
+      read_attribute(:name)
+    elsif project_id
+      "#{project.name}'s Project Account"
+    elsif person_id
+      "#{person.name}'s Project Account"
+    end
+  end
+  
+  def allocated_total
+    sum_allocations_less_commission(invoice_allocations)
+  end
+
+  def pending_total
+    sum_allocations_less_commission(invoice_allocations.pending)
+  end
+
+  def disbursed_total
+    sum_allocations_less_commission(invoice_allocations.disbursed)
+  end
+
+  private
+  def sum_allocations_less_commission(allocations)
+    allocations.inject(0) {|total,allocation| total += allocation.amount * (1 - allocation.commission)}
+  end
+
 end
