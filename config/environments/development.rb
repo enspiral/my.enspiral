e9926330bbe861f6e87b8af7a87cfeae03fff45c
PrototypeAlex <darth.gibson@gmail.com>
2012-01-12 13:46:47 +1300

    added notifications email tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,11 +14,19 @@ Enspiral::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.asset_host = 'http://localhost:3000'
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :host => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = { 
+    :address => "smtp.gmail.com", 
+    :port => 587,
+    :domain => 'localhost:3000',
+    :user_name => 'darth.gibson',
+    :password => 'Felixthelecturer',
+    :authentication => 'plain',
+    :enable_starttls_auto => true }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
