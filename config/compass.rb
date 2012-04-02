13a6fd254dd263c7ee1dbb5a46cd506ff55ba835
allansideas <allansideas@gmail.com>
2012-04-02 20:35:25 +1200

    blank slate for public face, update to rails 3.2.3

--- config/compass.rb
+++ config/compass.rb
@@ -6,8 +6,6 @@ project_type = :rails
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "intranet"
-css_path = "app/assets/stylesheets/intranet"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
