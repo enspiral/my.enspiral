2e6dce10ea81ea67ebfe5a951e46e8ffca4d7ad2
allansideas <allansideas@gmail.com>
2012-04-23 11:57:18 +1200

    update migration to work with PG

--- db/migrate/20120419205625_remove_null_from_paid.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNullFromPaid < ActiveRecord::Migration
-  def up
-    change_column :invoices, :paid, :boolean, null: false
-  end
-
-  def down
-    change_column :invoices, :paid, :boolean, null: true
-  end
-end
