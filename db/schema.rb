c79c7f439c9de7690f3162fe00b891a54185db00
Robert Guthrie <rguthrie@gmail.com>
2012-04-23 16:24:12 +1200

    ok fix migration bug

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
 
   create_table "accounts", :force => true do |t|
     t.decimal  "balance",    :precision => 10, :scale => 2, :default => 0.0
-    t.datetime "created_at",                                                  :null => false
-    t.datetime "updated_at",                                                  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
     t.boolean  "active",                                    :default => true
     t.boolean  "public"
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
   create_table "cities", :force => true do |t|
     t.integer  "country_id"
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "companies", :force => true do |t|
@@ -70,15 +70,15 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
 
   create_table "countries", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "customers", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "company_id"
   end
 
@@ -90,8 +90,8 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
     t.text     "summary"
     t.text     "content"
     t.datetime "published"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "funds_transfers", :force => true do |t|
@@ -112,8 +112,8 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
     t.decimal  "amount",     :precision => 10, :scale => 2
     t.string   "currency"
     t.boolean  "disbursed"
-    t.datetime "created_at",                                                 :null => false
-    t.datetime "updated_at",                                                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.decimal  "commission", :precision => 10, :scale => 2, :default => 0.2
     t.decimal  "hours",      :precision => 10, :scale => 2, :default => 0.0
     t.integer  "account_id"
@@ -126,8 +126,8 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
     t.boolean  "paid",                                          :default => false, :null => false
     t.date     "date"
     t.date     "due"
-    t.datetime "created_at",                                                       :null => false
-    t.datetime "updated_at",                                                       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "number"
     t.integer  "company_id"
     t.string   "xero_reference"
@@ -147,8 +147,8 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "job_title"
-    t.datetime "created_at",                                                                   :null => false
-    t.datetime "updated_at",                                                                   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "email"
     t.integer  "user_id"
     t.decimal  "base_commission",            :precision => 10, :scale => 2, :default => 0.2
@@ -172,18 +172,26 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
     t.integer  "account_id"
   end
 
+  create_table "people_accounts", :force => true do |t|
+    t.integer  "account_id"
+    t.integer  "person_id"
+    t.string   "role"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "people_skills", :force => true do |t|
     t.integer  "skill_id"
     t.integer  "person_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "project_bookings", :force => true do |t|
     t.date     "week"
     t.integer  "time"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "project_membership_id"
   end
 
@@ -192,21 +200,19 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
   create_table "project_memberships", :force => true do |t|
     t.integer  "person_id"
     t.integer  "project_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "is_lead"
     t.string   "role"
   end
 
-  add_index "project_memberships", ["person_id"], :name => "index_project_people_on_person_id"
   add_index "project_memberships", ["project_id", "person_id"], :name => "index_project_memberships_on_project_id_and_person_id", :unique => true
-  add_index "project_memberships", ["project_id"], :name => "index_project_people_on_project_id"
 
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",                                 :null => false
-    t.datetime "updated_at",                                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "customer_id"
     t.decimal  "budget",      :precision => 10, :scale => 2
     t.date     "due_date"
@@ -218,8 +224,8 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
 
   create_table "service_categories", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "services", :force => true do |t|
@@ -227,14 +233,14 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
     t.integer  "service_category_id"
     t.text     "description"
     t.float    "rate"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "skills", :force => true do |t|
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "transactions", :force => true do |t|
@@ -243,17 +249,17 @@ ActiveRecord::Schema.define(:version => 20120422094257) do
     t.decimal  "amount",      :precision => 10, :scale => 2
     t.string   "description"
     t.date     "date"
-    t.datetime "created_at",                                 :null => false
-    t.datetime "updated_at",                                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
     t.string   "username"
-    t.string   "email",                  :default => "",   :null => false
+    t.string   "email",                                    :null => false
     t.string   "encrypted_password"
     t.string   "password_salt"
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "role"
     t.string   "reset_password_token"
     t.datetime "remember_created_at"
