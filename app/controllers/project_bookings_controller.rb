9e6c7a6f9417c64bcc486f83a151fd909e817247
Allan Anderson <allansideas@gmail.com>
2012-05-02 21:04:54 +1200

    lots of changes from JV's cards

--- app/controllers/project_bookings_controller.rb
+++ app/controllers/project_bookings_controller.rb
@@ -22,13 +22,16 @@ class ProjectBookingsController < IntranetController
     @person = params[:person_id] ? Person.find(params[:person_id]) : current_user.person
     @project = Project.find(params[:project_id])
     @project_membership = ProjectMembership.find_by_project_id_and_person_id(@project.id, @person.id)
-
     if !@project_membership
       flash[:notice] = "No such Project Membership Exists for #{@person.name} in #{@project.name}."
       redirect_to projects_path
-    else
+    end
+    if current_person.company_admin_or_admin?(@project.company) or @project.project_memberships.where(is_lead: true).collect{|p| p.person == current_person}.include?(true) or @person == current_person
       @project_bookings = ProjectBooking.get_persons_project_bookings(@project_membership, params[:dates])
       @formatted_dates = ProjectBooking.get_formatted_dates(params[:dates])
+    else
+      flash[:notice] = "Not today captiain! You must be project lead, or admin to edit someone else's capacity."
+      redirect_to projects_path
     end
   end
 
