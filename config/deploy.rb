bd9366b4c2a69919be4f149baf3ab219d58eaa76
Allan Anderson <allansideas@gmail.com>
2012-05-03 09:53:57 +1200

    unnecessary stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ task :staging do
   set :branch,    "staging"
   set :rails_env, "staging"
   set :deploy_to, "/home/#{user}/staging"
-  
+
   role :web, domain
   role :app, domain
   role :db,  domain, :primary => true
@@ -41,19 +41,7 @@ namespace :deploy do
   task :symlink_configs do
     run %( cd #{release_path} &&
       ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml
-      ln -nfs #{shared_path}/config/production.sphinx.conf #{release_path}/config/#{branch}.sphinx.conf
     )
-  end
-
-  desc "Link up Sphinx's indexes."
-  task :symlink_sphinx_indexes, :roles => [:app] do
-    run "ln -nfs #{shared_path}/db/sphinx #{current_path}/db/sphinx"
-  end
-
-  desc "Activate Sphinx"
-  task :activate_sphinx, :roles => [:app] do
-    symlink_sphinx_indexes
-    thinking_sphinx.start
   end
 end
 
@@ -67,16 +55,6 @@ namespace :assets do
   end
 end
 
-before 'deploy', 'thinking_sphinx:stop'
-after 'deploy', 'deploy:activate_sphinx'
-after 'deploy:migrations', 'deploy:activate_sphinx'
-after :deploy, "assets:precompile"
-after "deploy:update_code" do
-  deploy.symlink_configs
-  deploy.bundle
-end
-
-
 
 require "./config/boot"
 require "bundler/capistrano"
@@ -88,42 +66,15 @@ require 'airbrake/capistrano'
 
 require 'thinking_sphinx/deploy/capistrano'
 
+before 'deploy:update_code', 'thinking_sphinx:stop'
+after 'deploy:update_code', 'thinking_sphinx:start'
 
-namespace :thinking_sphinx do
-  def rake(*tasks)
-    rails_env = fetch(:rails_env, "production")
-    rake = fetch(:rake, "rake")
-    tasks.each do |t|
-      run "if [ -d #{release_path} ]; then cd #{release_path}; else cd #{current_path}; fi; #{rake} RAILS_ENV=#{rails_env} #{t}"
-    end
-  end
-   
-  task :stop do
-    rake "thinking_sphinx:stop"
-  end
-  
-  task :start do
-    rake "thinking_sphinx:start"
-  end
-  
-  task :restart do
-    rake "thinking_sphinx:stop thinking_sphinx:start"
-  end
-
-  task :rebuild do
-    run "ln -nfs #{shared_path}/config/production.sphinx.conf #{release_path}/config/#{branch}.sphinx.conf"
-    stop
-    index
-    start
-  end
-  
-  task :index do
-    run "cd #{current_path} && /usr/local/bin/indexer --config /home/enspiral/staging/current/config/staging.sphinx.conf --all --rotate"    
-  end
-  
-  task :reindex do
-    run "cd #{current_path} && /usr/local/bin/indexer --config /home/enspiral/staging/current/config/staging.sphinx.conf --all --rotate"    
+namespace :sphinx do
+  desc "Symlink Sphinx indexes"
+  task :symlink_indexes, :roles => [:app] do
+    run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
   end
 end
 
+after 'deploy:finalize_update', 'sphinx:symlink_indexes'
 set :whenever_command, "bundle exec whenever"
