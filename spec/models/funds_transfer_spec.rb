7dd0d6b5827129be25394ca691ec19f83b8d2231
Allan Anderson <allansideas@gmail.com>
2012-05-07 11:20:32 +1200

    trying the tests and not winning

--- spec/models/funds_transfer_spec.rb
+++ spec/models/funds_transfer_spec.rb
@@ -1,35 +1,82 @@
 require 'spec_helper'
 
 describe FundsTransfer do
-  it {should validate_presence_of :author}
-  it {should validate_presence_of :source_account}
-  it {should validate_presence_of :destination_account}
+  before(:each) do
+    @ft = FundsTransfer.make
+    @ft.source_account = Account.make!
+    @ft.destination_account = Account.make!
+    @ft.source_account.balance = 3
+    @ft.destination_account.balance = 3
+    @ft.author = Person.make!
+    @ft.description = "Description"
+    @ft.amount = 3.0
+    @ft.save!
+  end
+
+  it 'should validate_presence_of :author' do
+    @ft.author = nil
+    @ft.valid?
+    @ft.should have(1).errors_on(:author)
+  end
+  it 'should validate_presence_of :source_account' do
+    @ft.source_account = nil
+    @ft.valid?
+    @ft.should have(1).errors_on(:source_account)
+  end
+  it 'should validate_presence_of :destination_account' do
+    @ft.destination_account = nil
+    @ft.valid?
+    @ft.should have(1).errors_on(:destination_account)
+  end
 
   it 'validates amount is greater than 0' do
-    subject.amount = 0
-    subject.valid?
-    subject.should have(1).errors_on(:amount)
+    @ft.amount = 0
+    @ft.valid?
+    #amount and invalid transaction
+    @ft.should have(2).errors_on(:amount)
 
-    subject.amount = -1
-    subject.valid?
-    subject.should have(1).errors_on(:amount)
+    @ft.amount = -1
+    @ft.valid?
+    @ft.should have(2).errors_on(:amount)
   end
 
   it 'creates source and destination transactions on create' do
     person = Person.make!
     user = person.user
-    source_account = Account.make!
-    destination_account = Account.make!
+    destination_account = Account.make
+    destination_account.balance = 3
+    destination_account.save
+    source_account = Account.make
+    source_account.balance = 3
+    source_account.save
+    source_account.min_balance = 0
+    destination_account.min_balance = 0
     person.accounts << source_account
     lambda{
-    @ft = FundsTransfer.make!(author: person,
+    @ftr = FundsTransfer.make!(author: person,
                              amount: 1.50,
                              source_account: source_account,
                              destination_account: destination_account,
                              description: 'test transfer')
     }.should change(Transaction, :count).by(2)
-    @ft.source_transaction.should_not be_nil
-    @ft.destination_transaction.should_not be_nil
+    @ftr.source_transaction.should_not be_nil
+    @ftr.destination_transaction.should_not be_nil
+  end
+
+  it 'validates that created transactions are not created if they will bring the account to below its min_balance' do
+    #hbnnnnnnnnnng
+    @ft.amount = -3
+    @ft.source_account.balance = 0
+    @ft.source_account.min_balance = 0
+    @ft.destination_account.balance = 0
+    @ft.destination_account.min_balance = 0
+    @ft.valid?
+    puts @ft.errors.inspect
+    @ft.should have(1).errors_on(:amount)
+    @ft.source_account.balance = 4
+    @ft.source_account.min_balance = 0
+    @ft.valid?
+    @ft.should have(0).errors_on(:amount)
   end
 
   #it 'validates author is owner of source_account on create' do
