0c4d9ae378023c99174b21bfd641c4742aaf03eb
Allan Anderson <allansideas@gmail.com>
2012-05-08 10:02:06 +1200

    vanity urls, main content for person show in marketing

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -50,9 +50,9 @@ SimpleForm.setup do |config|
     b.use :placeholder
     b.use :label
     b.wrapper :tag => 'div', :class => 'controls' do |ba|
+      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
       ba.use :input
       ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
     end
   end
 
