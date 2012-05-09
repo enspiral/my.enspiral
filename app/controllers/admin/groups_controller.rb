a4defaf8c6db37856f7659fe2aecb19abfa3ba40
Allan Anderson <allansideas@gmail.com>
2012-05-09 14:18:44 +1200

    views, and controller actiosn

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,19 +1,41 @@
 class Admin::GroupsController < ApplicationController
   def index
-  end
-
-  def edit
+    @groups = Group.order('description ASC')
   end
 
   def new
+    @group = Group.new
   end
 
-  def update
+  def edit
+    @group = Group.find(params[:id])
   end
 
   def create
+    @group = Group.new(params[:group])
+
+    if @group.save
+      flash[:notice] = 'group was successfully created.'
+      redirect_to admin_groups_url
+    else
+      render :action => "new" 
+    end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update_attributes(params[:group])
+      flash[:notice] = 'group was successfully updated.'
+      redirect_to admin_groups_url
+    else
+      render :action => "edit" 
+    end
   end
 
   def destroy
+    @group = Group.find(params[:id])
+    flash[:notice] = 'group was successfully deleted.'
+    redirect_to admin_groups_url
   end
 end
