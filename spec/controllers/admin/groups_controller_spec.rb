e96f99bfd2bdf6597a1fad762d76b15cd3b96eb5
Allan Anderson <allansideas@gmail.com>
2012-05-09 18:30:56 +1200

    group specs

--- spec/controllers/admin/groups_controller_spec.rb
+++ spec/controllers/admin/groups_controller_spec.rb
@@ -1,46 +1,51 @@
 require 'spec_helper'
 
 describe Admin::GroupsController do
-
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
+  it 'requires an admin' do
+    @person = Person.make!(:staff)
+    sign_in @person.user
+    get :index
+    response.should be_redirect
   end
 
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+  describe 'a system admin' do
+    before(:each) do
+      @person = Person.make!(:admin)
+      @group = Group.make!(name: "name")
+      sign_in @person.user
     end
-  end
 
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
+    it 'indexes groups' do
+      get :index
       response.should be_success
+      response.should render_template(:index)
+      assigns(:groups).should_not be_nil
     end
-  end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
+    it 'shows a new group form' do
+      get :new
       response.should be_success
+      response.should render_template :new
     end
-  end
 
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
+    it 'shows a edit group form' do
+      get :edit, id: @group.id
       response.should be_success
+      response.should render_template :edit
+    end
+
+    it 'creates groups' do
+      lambda{
+      post :create, group: {name: 'mcdonalds'}
+      }.should change(Group, :count).by(1)
+      response.should be_redirect
+    end
+
+    it 'updates groups' do
+      put :update, id: @group.id, group: {name: 'figgyies'}
+      response.should be_redirect
+      assigns(:group).description.should == 'figgyies'
     end
   end
 
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Group do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate presence of description" do
+    @group = Group.create
+    @group.should have(1).error_on(:name) 
+  end
+
+  it 'should delete associated people_group rows on destroy' do
+    @person = Person.make
+    @person.save
+    @group = Group.create(name: 'bobsgroup')
+    @group.save
+    @person.groups << @group
+    @person.groups.length.should == 1
+    lambda {
+      @group.destroy
+    }.should change(PeopleGroup, :count).by(-1)
+  end
 end
