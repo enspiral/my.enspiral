105810b73c7735db77f21483867279dbf2d380c2
Allan Anderson <allansideas@gmail.com>
2012-05-16 12:54:04 +1200

    make more suggested changes to blog

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,4 +2,5 @@ class BlogPost < ActiveRecord::Base
   attr_accessible :post_id, :author, :blog_id, :content, :posted_at, :summary, :title, :url
   validates_uniqueness_of :post_id
   belongs_to :blog
+  default_scope :order => "posted_at DESC"
 end
