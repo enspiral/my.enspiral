05aa489a1c8edcc24d28179d6f5a9d0588a4eb1b
Allan Anderson <allansideas@gmail.com>
2012-05-16 12:38:00 +1200

    fix blog spec

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -5,53 +5,21 @@ describe Blog do
     @blog = Blog.make
     @blog.feed_url = 'http://blog.enspiral.com/feed/'
     @blog.save!
+    feed_item = double("rss_item", title: "Title",
+                                  url:"http://blog.enspiral.com/2012/05/15/the-hundred-year-company/", 
+                                  published:"2012-05-15 10:24:06 UTC",
+                                  author:"Will L.",
+                                  categories:["Opinion", "Reflections", "Start-ups"],
+                                  entry_id:"http://blog.enspiral.com/?p=703",
+                                  summary:"I&rsquo;m a bit of a business geek. Ever since I threw in my Engineering job to try my hand starting my own web design business in the sexy dot.com age, and later",
+                                  content:"<p style=\"text-align: center;\"><a href=\"http://blog.enspiral.com/wp-content/uploads/2012/05/100-year-old-company.jpg\"><img class=\"size-full wp-image-724 aligncenter\" title=\"100-year-old-company\" src=\"http://blog.enspiral.com/wp-content/uploads/2012/05/100-year-old-company.jpg\" alt=\"\" width=\"529\" height=\"447\"></a></p>"
+                         ).as_null_object
+    parser = double("feed_parser", title: 'Test Blog', url:'www.url.url', description: 'description', entries: [feed_item])
 
+    Feedzirra::Feed.stub(:fetch_and_parse).and_return(parser)
+    
     lambda {
-      feed = Feedzirra::Feed.fetch_and_parse @blog.feed_url
-      
-      return unless feed.respond_to?(:entries)
-      
-      entry = feed.entries.first
-      entry.sanitize!
-      BlogPost.create :blog_id => @blog.id,
-                  :post_id => entry.id,
-                  :title => entry.title,
-                  :url => entry.url,
-                  :author => entry.author,
-                  :summary => entry.summary,
-                  :content => entry.content,
-                  :posted_at => entry.published
-    }.should change(BlogPost, :count).by(1)
-  end
-
-  it 'should not create duplicate blog post items' do
-    @blog = Blog.make
-    @blog.feed_url = 'http://blog.enspiral.com/feed/'
-    @blog.save!
-
-    lambda {
-      feed = Feedzirra::Feed.fetch_and_parse @blog.feed_url
-      
-      return unless feed.respond_to?(:entries)
-      
-      entry = feed.entries.first
-      entry.sanitize!
-      BlogPost.create :blog_id => @blog.id,
-                  :post_id => entry.id,
-                  :title => entry.title,
-                  :url => entry.url,
-                  :author => entry.author,
-                  :summary => entry.summary,
-                  :content => entry.content,
-                  :posted_at => entry.published
-      BlogPost.create :blog_id => @blog.id,
-                  :post_id => entry.id,
-                  :title => entry.title,
-                  :url => entry.url,
-                  :author => entry.author,
-                  :summary => entry.summary,
-                  :content => entry.content,
-                  :posted_at => entry.published
+      @blog.get_updated_posts
     }.should change(BlogPost, :count).by(1)
   end
 
