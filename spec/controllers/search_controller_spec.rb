00b5ec2ce9fc8f064fb4d687225534257094112d
Allan Anderson <allansideas@gmail.com>
2012-05-17 13:09:42 +1200

    fix search spec

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe SearchController do
+  before(:each) do
+    @person = Person.make!(:staff)
+    sign_in @person.user
+  end
+    
 
   describe "GET 'index'" do
     it "returns http success" do
