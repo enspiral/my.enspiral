1b4ef2f7e1de8efbab54a78acc77e6fe186a8395
Allan Anderson <allansideas@gmail.com>
2012-05-22 20:37:29 +1200

    multiple image upload for projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120522010748) do
+ActiveRecord::Schema.define(:version => 20120522015458) do
 
   create_table "accounts", :force => true do |t|
     t.decimal  "balance",     :precision => 10, :scale => 2, :default => 0.0
@@ -312,6 +312,13 @@ ActiveRecord::Schema.define(:version => 20120522010748) do
   end
 
   add_index "projects", ["slug"], :name => "index_projects_on_slug", :unique => true
+
+  create_table "projects_images", :force => true do |t|
+    t.integer  "project_id"
+    t.string   "image_uid"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "service_categories", :force => true do |t|
     t.string   "name"
---
+++ spec/models/projects_image_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe ProjectsImage do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
