38bfec30126feb4f06687daccd1862de1d6b6af8
Robert Guthrie <rguthrie@gmail.com>
2012-06-12 16:25:06 +1200

    fix specs

--- spec/controllers/your_capacity_controller_spec.rb
+++ spec/controllers/your_capacity_controller_spec.rb
@@ -20,11 +20,6 @@ describe YourCapacityController do
       get :index, project_id: @project.id
       response.should be_success
     end
-    it 'edit' do
-      get :edit, project_id: @project.id
-      response.should be_success
-    end
-
     it 'update creates project_bookings' do
       lambda{
         put :update, project_bookings: [{project_membership_id: @project_membership.id,
@@ -33,15 +28,6 @@ describe YourCapacityController do
       response.should be_redirect
       ProjectBooking.last.project_membership.project.should == @project
     end
-
-    it 'update updates project_bookings' do
-      lambda{
-        put :update, project_bookings: [{project_membership_id: pb.project_membership_id,
-                                       week: pb.week, time: 5}]
-      }.should_not change(ProjectBooking, :count)
-      response.should be_redirect
-    end
-
   end
 end
 
