477dbadf9c0853678b135c17d4db5fe927601a20
Allan Anderson <allansideas@gmail.com>
2012-07-09 23:30:00 +1200

    try a config option for modernizr error and add new logo for andrew to look at

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -44,7 +44,7 @@ Enspiral::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   
-  config.assets.precompile += %w( intranet.css intranet.js marketing.css marketing.js )
+  config.assets.precompile += %w( intranet.css intranet.js marketing.css marketing.js modernizr-2.5.3.js )
   #config.assets.precompile += %w( *.js *.css )
 
   # Disable delivery errors, bad email addresses will be ignored
