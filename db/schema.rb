f2773dbadfc3ec1be639f0bde8fbbc0667242293
Joshua Vial <joshua@enspiral.com>
2013-03-09 07:45:51 +1300

    fixing conflict

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
 
   create_table "accounts", :force => true do |t|
     t.decimal  "balance",     :precision => 10, :scale => 2, :default => 0.0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                    :null => false
+    t.datetime "updated_at",                                                    :null => false
     t.string   "name"
     t.string   "category"
     t.boolean  "active",                                     :default => true
@@ -63,8 +63,8 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
   create_table "cities", :force => true do |t|
     t.integer  "country_id"
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "companies", :force => true do |t|
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
     t.string   "tagline"
     t.integer  "outgoing_account_id"
     t.boolean  "visible",                                             :default => true
+    t.string   "kind"
+    t.boolean  "show_projects",                                       :default => true
   end
 
   add_index "companies", ["active"], :name => "index_companies_on_active"
@@ -111,15 +113,15 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
 
   create_table "countries", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "customers", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "company_id"
   end
 
@@ -140,8 +142,8 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
     t.text     "summary"
     t.text     "content"
     t.datetime "published"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "funds_transfer_template_lines", :force => true do |t|
@@ -190,8 +192,8 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
     t.decimal  "amount",       :precision => 10, :scale => 2
     t.string   "currency"
     t.boolean  "disbursed",                                   :default => false, :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                     :null => false
+    t.datetime "updated_at",                                                     :null => false
     t.decimal  "contribution", :precision => 10, :scale => 2, :default => 0.2
     t.decimal  "hours",        :precision => 10, :scale => 2, :default => 0.0
     t.integer  "account_id"
@@ -204,8 +206,8 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
     t.boolean  "paid",                                          :default => false, :null => false
     t.date     "date"
     t.date     "due"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                       :null => false
+    t.datetime "updated_at",                                                       :null => false
     t.integer  "number"
     t.integer  "project_id"
     t.string   "xero_reference"
@@ -231,8 +233,8 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "job_title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                                   :null => false
+    t.datetime "updated_at",                                                                   :null => false
     t.string   "email"
     t.integer  "user_id"
     t.decimal  "base_commission",            :precision => 10, :scale => 2, :default => 0.2
@@ -274,15 +276,15 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
   create_table "people_skills", :force => true do |t|
     t.integer  "skill_id"
     t.integer  "person_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "project_bookings", :force => true do |t|
     t.date     "week"
     t.integer  "time"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
     t.integer  "project_membership_id"
   end
 
@@ -291,19 +293,21 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
   create_table "project_memberships", :force => true do |t|
     t.integer  "person_id"
     t.integer  "project_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.boolean  "is_lead"
     t.string   "role"
   end
 
+  add_index "project_memberships", ["person_id"], :name => "index_project_people_on_person_id"
   add_index "project_memberships", ["project_id", "person_id"], :name => "index_project_memberships_on_project_id_and_person_id", :unique => true
+  add_index "project_memberships", ["project_id"], :name => "index_project_people_on_project_id"
 
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
     t.integer  "customer_id"
     t.decimal  "budget",        :precision => 10, :scale => 2
     t.date     "due_date"
@@ -329,25 +333,18 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "service_categories", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "services", :force => true do |t|
-    t.integer  "person_id"
-    t.integer  "service_category_id"
     t.text     "description"
-    t.float    "rate"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "name"
+    t.string   "image_uid"
   end
 
   create_table "skills", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "transactions", :force => true do |t|
@@ -356,17 +353,17 @@ ActiveRecord::Schema.define(:version => 20120911010546) do
     t.decimal  "amount",      :precision => 10, :scale => 2
     t.string   "description"
     t.date     "date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
   end
 
   create_table "users", :force => true do |t|
     t.string   "username"
-    t.string   "email",                                    :null => false
+    t.string   "email",                  :default => "",   :null => false
     t.string   "encrypted_password"
     t.string   "password_salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
     t.string   "role"
     t.string   "reset_password_token"
     t.datetime "remember_created_at"
