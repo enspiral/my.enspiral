bb77d854ee4b3c1ace68f23949f5df3ad5b55016
Reaksmey <reaksmey@instedd.org>
2013-11-11 06:48:54 +0700

    improve performance of import invoice from xero

--- app/models/company.rb
+++ app/models/company.rb
@@ -62,7 +62,8 @@ class Company < ActiveRecord::Base
   end
 
   def get_invoice_from_xero_and_update
-      invoices = self.xero.Invoice.all
+      from = Invoice.where("xero_reference <> ''").first.date.beginning_of_day.to_s.split(" ")[0..1].join("T")
+      invoices = self.xero.Invoice.all(:where => {:date_is_greater_than_or_equal_to => DateTime.parse(from)})
       Invoice.insert_new_invoice invoices
   end
 
