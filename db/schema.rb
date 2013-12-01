ba11f5a67a4a32bdb10247df4da06ec6e1ac665b
Reaksmey <reaksmey@instedd.org>
2013-12-01 13:36:12 +0700

    approv customer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131110202516) do
+ActiveRecord::Schema.define(:version => 20131201061318) do
 
   create_table "accounts", :force => true do |t|
     t.decimal  "balance",     :precision => 10, :scale => 2, :default => 0.0
@@ -121,9 +121,10 @@ ActiveRecord::Schema.define(:version => 20131110202516) do
   create_table "customers", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
     t.integer  "company_id"
+    t.boolean  "approved",    :default => true
   end
 
   create_table "featured_items", :force => true do |t|
