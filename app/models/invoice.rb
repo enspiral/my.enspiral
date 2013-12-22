94a14d5a13ec12154450022d0d61a569b4243574
CHEA REAKSMEY <chea.reaksmey@gmail.com>
2013-12-22 16:15:06 +0700

    draft, paid invoice should not import and show imported invoices window

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -111,9 +111,18 @@ class Invoice < ActiveRecord::Base
       date = inv.date
       currency = inv.currency_code
       due_date = inv.due_date
-      if xero_ref && customer && amount && date && due_date && currency != "NZD"
-        Invoice.create!(:customer_id => customer.id, :amount => amount, :date => date, :due => due_date, :xero_reference => xero_ref, :company_id => company_id, :approved => false, :currency => currency) unless Invoice.find_by_xero_reference(xero_ref)
+      status = inv.status
+      if xero_ref && customer && amount && date && due_date && currency != "NZD" && status == "VOIDED"
+        Invoice.create!(:customer_id => customer.id, :amount => amount, :date => date, :due => due_date, :xero_reference => xero_ref, :company_id => company_id, :approved => false, :currency => currency, :imported => true) unless Invoice.find_by_xero_reference(xero_ref)
       end
+    end
+  end
+
+  def self.refresh_imported_invoice
+    invoices = Invoice.where(:imported => true)
+    invoices.each do |inv|
+      inv.imported = false
+      inv.save!
     end
   end
 
