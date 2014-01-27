1bb568a95ef887fce9d23564e858a59f99a666f0
reaksmey <reaksmey@enspiral.com>
2014-01-27 07:55:18 +0700

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131222234006) do
+ActiveRecord::Schema.define(:version => 20140127003625) do
 
   create_table "accounts", :force => true do |t|
     t.decimal  "balance",     :precision => 10, :scale => 2, :default => 0.0
