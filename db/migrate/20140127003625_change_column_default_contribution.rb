87b8b32d1720d73144c037e8eff44bbb8ecf035d
reaksmey <reaksmey@enspiral.com>
2014-02-03 06:16:45 +0700

    allocation percentage should be 3 decimal places

--- db/migrate/20140127003625_change_column_default_contribution.rb
+++ db/migrate/20140127003625_change_column_default_contribution.rb
@@ -1,7 +1,9 @@
 class ChangeColumnDefaultContribution < ActiveRecord::Migration
   def up
+  	change_column :companies, :default_contribution, :decimal, :precision => 10, :scale => 3, :default => 0.20
   end
 
   def down
+  	change_column :companies, :default_contribution, :decimal, :precision => 10, :scale => 2, :default => 0.2
   end
 end
