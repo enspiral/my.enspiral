482219ff7018dd3ed0c0370dcc3168343c0b070a
reaksmey <reaksmey@enspiral.com>
2014-03-03 05:55:52 +0700

    import invoice with two months old

--- app/models/company.rb
+++ app/models/company.rb
@@ -85,7 +85,7 @@ class Company < ActiveRecord::Base
       xero_ref = Invoice.where(:imported => true).first.xero_reference
       xero_invoice = self.xero.Invoice.find("INV-#{xero_ref}")
       if xero_invoice
-        xero_date = (xero_invoice.date - 1.month).beginning_of_month
+        xero_date = (xero_invoice.date - 2.month).beginning_of_month
       end
       invoices = self.xero.Invoice.all(:where => {:date_is_greater_than_or_equal_to => xero_date, :type => "ACCREC"})
       Invoice.update_existed_invoice invoices
