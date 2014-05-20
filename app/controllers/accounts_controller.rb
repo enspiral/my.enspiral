42204d7ac958f6868d63904ecc12b83fac37be05
reaksmsey1 <chea.reaksmey@gmail.com>
2014-05-20 08:39:48 +0700

    update report

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -62,7 +62,6 @@ class AccountsController < IntranetController
 
   def update
     #check here for overdraft permissions
-    binding.pry
     if @account.update_attributes(params[:account])
       flash[:notice] = 'Updated Account'
       redirect_to [@company, @account]
