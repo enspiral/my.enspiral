9a170adde61f4d660dfc9e6c120687fcfdac3e44
reaksmsey1 <chea.reaksmey@gmail.com>
2014-06-16 16:20:22 +0700

    testing -- add action mailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -20,5 +20,10 @@ class Notifier < ActionMailer::Base
     @person = person
     mail :to => @person.email, :from => 'no-reply@enspiral.com', :subject => '[enspiral] Your Project Bookings'
   end
+
+  def send_welcome
+    # @notice = notice
+    mail :to => "reaksmey@enspiral.com", :from => "gnome@enspiral.com", :subject => "you have registered"
+  end
   
 end
