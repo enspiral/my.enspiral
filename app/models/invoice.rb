6c928d533237a4e8a85b449115c91a12366944f4
reaksmsey1 <chea.reaksmey@gmail.com>
2015-06-18 15:03:05 +0700

    ugly add code to fix the bug

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -159,55 +159,59 @@ class Invoice < ActiveRecord::Base
     allocation_currency = ""
     inv.line_items.each do |el|
       if el.attributes[:line_amount] < 0
-        allocation_personal_with_discount = el.tracking[1].option
-        allocation_amount_with_discount = el.attributes[:line_amount]
+        allocation_personal_with_discount = el.tracking[1].option if !el.tracking.empty?
+        allocation_amount_with_discount = allocation_amount_with_discount + el.attributes[:line_amount]
       end
     end
 
     inv.line_items.each do |el|
-      if el.tracking[1].option == allocation_personal_with_discount && el.attributes[:line_amount] > 0
-        total_positive_line_item = total_positive_line_item + el.attributes[:line_amount]
+      if !el.tracking.empty?
+        if el.tracking[1].option == allocation_personal_with_discount && el.attributes[:line_amount] > 0
+          total_positive_line_item = total_positive_line_item + el.attributes[:line_amount]
+        end
       end
     end
     total_line_item_with_discount = total_positive_line_item + allocation_amount_with_discount
 
     inv.line_items.each do |el|
-      allocation_team = el.tracking[0].option
-      allocation_personal = el.tracking[1].option
-      allocation = allocation_personal.split("-")
-      allocation_currency = inv.currency_code
-      if el.tracking[1].option == allocation_personal_with_discount && el.attributes[:line_amount] > 0
-         line_percentage = el.attributes[:line_amount] / total_positive_line_item
-         line_discount_value = line_percentage * allocation_amount_with_discount
-         allocation_amount = el.attributes[:line_amount] + line_discount_value
-      end
-      if Account.find_by_name("#{allocation[0]}'s Enspiral Account")
-        allocation_account = Account.find_by_name("#{allocation[0]}'s Enspiral Account")
-      elsif Account.find_by_name("#{allocation[0]}'s Enspiral account")
-        allocation_account = Account.find_by_name("#{allocation[0]}'s Enspiral account")
-      elsif Account.find_by_name("#{allocation[0]}'s Enspiral Services account")
-        allocation_account = Account.find_by_name("#{allocation[0]}'s Enspiral Services account")
-      elsif Account.find_by_name("#{allocation[0]}")
-        allocation_account = Account.find_by_name("#{allocation[0]}")
-      end
-      allocation_team_account = Account.find_by_name("TEAM: #{allocation_team}")
-      allocation_contribution = allocation[1].to_i / 100.0
-      if el.attributes[:line_amount] > 0 && allocation_amount && allocation_account && allocation_account.closed == false && allocation_contribution && allocation_team_account
-        if allocation_amount > 0
-          InvoiceAllocation.create!(:invoice_id => saved_invoice.id, 
-                                    :amount => allocation_amount, 
-                                    :currency => allocation_currency, 
-                                    :contribution => allocation_contribution, 
+      if !el.tracking.empty?
+        allocation_team = el.tracking[0].option
+        allocation_personal = el.tracking[1].option
+        allocation = allocation_personal.split("-")
+        allocation_currency = inv.currency_code
+        if el.tracking[1].option == allocation_personal_with_discount && el.attributes[:line_amount] > 0
+           line_percentage = el.attributes[:line_amount] / total_positive_line_item
+           line_discount_value = line_percentage * allocation_amount_with_discount
+           allocation_amount = el.attributes[:line_amount] + line_discount_value
+        end
+        if Account.find_by_name("#{allocation[0]}'s Enspiral Account")
+          allocation_account = Account.find_by_name("#{allocation[0]}'s Enspiral Account")
+        elsif Account.find_by_name("#{allocation[0]}'s Enspiral account")
+          allocation_account = Account.find_by_name("#{allocation[0]}'s Enspiral account")
+        elsif Account.find_by_name("#{allocation[0]}'s Enspiral Services account")
+          allocation_account = Account.find_by_name("#{allocation[0]}'s Enspiral Services account")
+        elsif Account.find_by_name("#{allocation[0]}")
+          allocation_account = Account.find_by_name("#{allocation[0]}")
+        end
+        allocation_team_account = Account.find_by_name("TEAM: #{allocation_team}")
+        allocation_contribution = allocation[1].to_i / 100.0
+        if el.attributes[:line_amount] > 0 && allocation_amount && allocation_account && allocation_account.closed == false && allocation_contribution && allocation_team_account
+          if allocation_amount > 0
+            InvoiceAllocation.create!(:invoice_id => saved_invoice.id, 
+                                      :amount => allocation_amount, 
+                                      :currency => allocation_currency, 
+                                      :contribution => allocation_contribution, 
+                                      :account_id => allocation_account.id,
+                                      :team_account_id => allocation_team_account.id)
+          else
+            inv_allocation = InvoiceAllocation.where(:invoice_id => saved_invoice.id,
                                     :account_id => allocation_account.id,
                                     :team_account_id => allocation_team_account.id)
-        else
-          inv_allocation = InvoiceAllocation.where(:invoice_id => saved_invoice.id,
-                                  :account_id => allocation_account.id,
-                                  :team_account_id => allocation_team_account.id)
-          
-          if inv_allocation.count > 0
-            inv_allocation[0].amount = inv_allocation[0].amount + allocation_amount
-            inv_allocation[0].save!
+            
+            if inv_allocation.count > 0
+              inv_allocation[0].amount = inv_allocation[0].amount + allocation_amount
+              inv_allocation[0].save!
+            end
           end
         end
       end
@@ -366,7 +370,7 @@ class Invoice < ActiveRecord::Base
           end
         end
       end
-      amount = inv.attributes[:sub_total]
+      amount = inv.attributes[:sub_total] > 0 ? inv.attributes[:sub_total] : 0
       date = inv.date
       currency = inv.currency_code
       due_date = inv.due_date
