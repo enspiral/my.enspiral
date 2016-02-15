56b4348f17a47869d7448f06b7138fa5e66a1520
charlie-ablett <im.charlie.ablett@gmail.com>
2016-02-15 21:00:42 +1300

    Account allocation view reflects correct date, cleaner formatting

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -50,7 +50,8 @@ class Invoice < ActiveRecord::Base
   end
 
   def overdue?
-    Date.today > due
+    # invoice_allocation.invoice.due < Date.today.in_time_zone(invoice_allocation.company.time_zone)
+    Time.now.in_time_zone(company.time_zone).to_date > due
   end
 
   def reference
