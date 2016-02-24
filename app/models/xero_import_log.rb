3e775f94fa9ab02e49ff91de3cba04d680147c04
charlie-ablett <im.charlie.ablett@gmail.com>
2016-02-24 23:30:57 +1300

    Improve dashboard

--- app/models/xero_import_log.rb
+++ app/models/xero_import_log.rb
@@ -6,6 +6,8 @@ class XeroImportLog < ActiveRecord::Base
   belongs_to :company
   belongs_to :person
 
+  validates_presence_of :company
+
   def author_name
     return "system" if person.nil?
     person.name
